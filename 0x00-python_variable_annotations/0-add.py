b707a21b0f14623ae8da5cfe647335b58d78ce88
Hussein380 <huznigarane@gmail.com>
2024-08-01 08:25:00 +0300

    added files for task 0: function that takes float a and b and returns their sum which is float

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,14 @@
+
+#!/usr/bin/env python3
+"""
+add: takes a float a and float b an returns their sun
+args:
+    a: float  argument
+    b: float argument
+returns: float sum
+
+"""
+
+def add (a: float, b: float) -> float:
+    return a + b
+
