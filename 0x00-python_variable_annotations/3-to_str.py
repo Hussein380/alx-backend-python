a773b3d233b0ab20250841b55f08b05efc272af5
Hussein380 <huznigarane@gmail.com>
2024-08-02 08:52:04 +0300

    updated task 1, 2 and 3 with right code logic by removing earlier ones

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
+'''Module for task 3.
+'''
 
 
 def to_str(n: float) -> str:
-    """
-    Cast a floating number in to a string
-    """
+    '''Cast a floting type number in to a string.
+    '''
     return str(n)
