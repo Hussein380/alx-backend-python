8c5082eaa989bcb969470df76538ed7f2462787d
Hussein380 <huznigarane@gmail.com>
2024-08-02 08:13:26 +0300

    removed comments of the function in this line which was above the functrion defination

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,13 +1,9 @@
 #!/usr/bin/env python3
-"""
-sum_list : annotated function which taakes input_list of loats as arg
-Args:
-    input_list  list of floats
-Return: sum of float
-"""
-
 from typing import List
 
 
 def sum_list(input_list: list[float]) -> float:
+    """
+    computes sum of a list of floating-point number
+    """
     return float(sum(input_list))
